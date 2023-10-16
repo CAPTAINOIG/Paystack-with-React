03e35e06edc0e5bd1dcd5adb6f35ef7a9b530586
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-10-16 15:30:29 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Paystackintegration from './component/Paystackintegration'
+
+function App() {
+  
+
+  return (
+    <>
+     <Paystackintegration/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Paystackintegration.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import PaystackPop from '@paystack/inline-js'
+
+
+const Paystackintegration = () => {
+  const [email, setEmail] = useState("")
+  const [amount, setAmount] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+
+  const payWithPayStack =(e)=>{
+    e.preventDefault()
+     const paystack = new PaystackPop()
+     paystack.newTransaction({
+      key: "pk_test_735675141fc133bb647f731aa99e663160380c80",
+      amount: amount * 100,
+      email,
+      firstName,
+      lastName,
+      onSuccess(transaction){
+        let message = `Payment Complete! Reference ${transaction.reference}`
+        alert(message)
+        setEmail("")
+        setAmount("")
+        setFirstName("")
+        setLastName("")
+      },
+      oncancel(){
+        alert("You have Canceled the transaction")
+      }
+     })
+  }
+
+  return (
+    <div className='w3-container w3-row '>
+      <div className='w3 container w3-purple'>
+        <h3 className='w3-center'>Make Payment</h3>
+      </div>
+      <div className='w3-container w3-quarter'></div>
+      <div className='w3-container w3-half'>
+        <div className='w3-container w3-card-4'>
+          <form id='paymentForm' className=''>
+            <div className='form-group'>
+              <label htmlFor="email">Email Address</label>
+              <input type="email" id='email-address' className='w3-input' onChange={(e) => setEmail(e.target.value)} value={email} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor="amount">Amount</label>
+              <input type="tel" id='amount' className='w3-input' onChange={(e) => setAmount(e.target.value)} value={amount} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor="first-name">First Name</label>
+              <input type="first-name" id='first-name' className='w3-input' onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor="last-name">Last Name</label>
+              <input type="last-name" id='last-name' className='w3-input' onChange={(e) => setLastName(e.target.value)} value={lastName} />
+            </div>
+            <button className='w3-btn w3-block w3-purple' type='submit' onClick={payWithPayStack}>PAY</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Paystackintegration
